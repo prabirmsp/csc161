7c658aa20e749812b57986e37006c2af5909f65f
Prabir Pradhan <prabirmsp@gmail.com>
2015-04-18 21:46:02 -0500

    sup4

--- sup-prob-4.c
+++ sup-prob-4.c
@@ -27,6 +27,8 @@ void printAllItems (struct item * first);
 
 void search (struct item ** first);
 
+void returnBook (struct item ** first);
+
 int main () {
 	// print title
 	printf("\nLibrary Books - Supplemental Problem 4\n"
@@ -60,6 +62,7 @@ int main () {
 			search(&first);
 			break;
 			case 'r': case 'R': //  return
+			returnBook(&first);
 			break;
 			case 'q': case 'Q': //  quit
 				printf("Program terminated.\n");
@@ -73,15 +76,18 @@ int main () {
 
 
 void getStringInput (char * str) {
-	scanf("%s", str);
-	while (getchar()!='\n');
-	/*
+	//scanf("%s", str);
+	//while (getchar()!='\n');
+	
 	char c;
 	int i = 0;
-	while (c = getchar() != '\n') {
-		str[i] = c;
-		i++;
-	}*/
+	while ((c = getchar()) != '\n') {
+		str[i++] = c;
+	}
+	str[i] = 0;
+
+	for (int j = 0; str[j]; j++) // convert to lowercase
+		str[j] = tolower(str[j]);
 }
 
 void checkout (struct item ** first) {
@@ -89,24 +95,24 @@ void checkout (struct item ** first) {
 
 	// get data from input
 	char username[MAX_STR];
-	printf("Enter the name of the user: ");
+	printf("Username: ");
 	getStringInput(username);
 	currentP = *first;
 
 	// search for the user in the list
-		
 	while (currentP && strcmp(username, currentP->name)) { // search for user
 		currentP = currentP->next;
 	}
 	if (currentP && !strcmp(username, currentP->name)) { // if existing user found
-		printf("Existing user found.\n");
+			printf("Existing user found with %d books checked out.\n"
+				   "Return all books to check out more books.\n");
 	}
 	else { // if no existing user found - create new user
-		printf("Number of books to check out (1-5): ");
+		printf("Number of books to check out (1-%d): ", maxBooks);
 		int books;
 		scanf("%d", &books);
 		while (getchar() != '\n'); // clear input stream until newline
-		if (books > 0 && books <= 5) {
+		if (books > 0 && books <= maxBooks) {
 			struct item * newItem = (struct item *) malloc(sizeof(struct item)); // create new item
 			strcpy(newItem->name, username); // set the name
 			newItem->numBooks = books; // set number of books checked out
@@ -155,7 +161,7 @@ void search (struct item ** first) {
 	struct item * previousP1;
 	struct item * previousP2;
 	char username[MAX_STR];
-	printf("Enter the name to search: ");
+	printf("Username: ");
 	getStringInput(username);
 	currentP = *first;
 	previousP1 = first;
@@ -180,4 +186,58 @@ void search (struct item ** first) {
 	}
 	else 
 		printf("No user found by the name \"%s\".\n", username);
+}
+
+void returnBook (struct item ** first) {
+	struct item * currentP;
+	struct item * previousP1;
+	char username[MAX_STR];
+	printf("Username: ");
+	getStringInput(username);
+	currentP = *first;
+	// search for the user in the list
+	while (currentP && strcmp(username, currentP->name)) {
+		// move to the next item
+		previousP1 = currentP;
+		currentP = currentP->next;
+	}
+	if (currentP && !strcmp(username, currentP->name)) { // if existing user found
+		char title [MAX_STR];
+		printf("Title: ");
+		getStringInput(title);
+		int i = 0;
+		while ((i < currentP->numBooks) && strcmp(title, currentP->booksBorrowed[i]))
+			i++;
+		if (!strcmp(title, currentP->booksBorrowed[i])) { // checked out book found
+			printf("booksoufn");
+			if (currentP->numBooks == 1) { // if only 1 book checked out
+				// DELETE USER
+				if (currentP == *first) // user is first in list
+					*first = currentP->next;
+				else // user is not first in list
+					previousP1->next = currentP->next;
+				free(currentP); // delete user
+				printf("This user has no more books checked out.\n");
+			}
+			else { // more than 1 book checked out
+				printf("LLLSLS");
+				if (i != currentP->numBooks-1) { // if it's not the last book in array
+					while (i < currentP->numBooks-1) { 
+						for(int j = 0; j < MAX_STR; j++) // shift the book titles down
+							currentP->booksBorrowed[i][j] = currentP->booksBorrowed[i+1][j];
+						i++;
+					}
+				}
+				for (int j = 0; j < MAX_STR; j++) // delete the last book title
+					currentP->booksBorrowed[i][j] = 0;
+				currentP->numBooks = currentP->numBooks-1;
+				printf("The book \"%s\" has been returned.\n", title);
+			}
+		}
+		else // book not found
+			printf("That book was not found.\n");
+		
+	}
+	else // user not found
+		printf("There are no users by that name. \n");
 }
